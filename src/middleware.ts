0899e9b83799afbf70fcbec7e3ebac9e60e35faa
Dlsmiuel <migueldls.mb@gmail.com>
2023-03-30 09:22:46 -0400

    added clerk (not finished yet)

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export default withClerkMiddleware((req) => {
+  console.log("hi from middleware");
+  return NextResponse.next();
+});
+
+// Stop Middleware running on static files
+export const config = {
+  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+};
